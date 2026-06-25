321adab7b9b3f64c7cafe6c8e1a1e623bb8fe0e1
gaesyeah <gabrielgoesrios@gmail.com>
2026-06-25 00:05:37 -0300

    chore: app module db connection configuration changed to url

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
+        url: config.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: config.get('NODE_ENV') !== 'production',
         ssl:
