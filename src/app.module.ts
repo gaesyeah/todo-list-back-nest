c6bb877f78f25bbf1ab77f24c12f96a8a0fffb13
gaesyeah <gabrielgoesrios@gmail.com>
2026-07-08 18:36:33 -0300

    refactor: remove unused app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -17,7 +15,5 @@ import { HttpQueryModule } from './http-query';
     AuthModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
